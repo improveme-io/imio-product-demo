80de71cccd06cd4b9bf4eb793dc86111cd932c0c
robotkutya <mihalyfuredi@gmail.com>
2023-04-12 21:31:18 +0200

    feat: updates favicon

---
+++ src/components/page-head.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+type PageHeadProps = {
+  title?: string;
+  description?: string;
+};
+
+export const PageHead = ({
+  title = "",
+  description = "Request, author and own your feedback",
+}: PageHeadProps) => {
+  return (
+    <Head>
+      <title>{`${title} ~ ImproveMe`}</title>
+      <meta name="description" content={description} />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
--- src/pages/feedback/create.tsx
+++ src/pages/feedback/create.tsx
@@ -1,17 +1,14 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { useUser } from "@clerk/nextjs";
+import { PageHead } from "~/components/page-head";
 
 const Create: NextPage = () => {
   const user = useUser();
 
   return (
     <>
-      <Head>
-        <title>feedback me</title>
-        <meta name="description" content="request, author and own feedback" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <PageHead title="Create" />
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-gray-200 to-gray-800">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
