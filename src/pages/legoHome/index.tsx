f65ea1e572c17fcb8569ea63ab5d634ca14d3877
Kristof Gatter <mail@kristofgatter.com>
2023-05-10 10:18:58 +0200

    feat(legoHome): add header and responsiveness

--- src/pages/legoHome/index.tsx
+++ src/pages/legoHome/index.tsx
@@ -11,21 +11,39 @@ const LegoHome: NextPage = () => {
   return (
     <>
       <PageHead title="improveme.io | Home" />
-      <main className="container mx-auto flex min-h-screen flex-col gap-x-8 bg-stone-50 px-16 py-8">
-        <h1 className="my-10 font-serif text-4xl">
+      <header className="flex justify-between bg-stone-50 px-8 py-8">
+      <Image
+            className=""
+            src="/HeroLogo.svg"
+            width={312/2}
+            height={153/2}
+            alt="Graphic depicting three people giving each other feedback in the cloud"
+          />
+        <div className="flex">
+          <Button
+            className="mr-2"
+            variant={"ghost"}
+            size={"sm"}>Log In</Button>
+          <Button
+            variant={"outline"}
+            size={"sm"}>Sign Up</Button>
+        </div>
+      </header>
+      <main className="container mx-auto flex min-h-screen flex-col gap-x-8 bg-stone-50 py-8 px-8 sm:px-16 md:pl-32">
+        <h1 className="my-12 font-serif text-4xl">
           Own Your Feedback, Own Your Growth
         </h1>
-        <div className="grid grid-cols-2">
+        <div className="md:grid md:grid-cols-2">
           <div>
             <h3 className="mb-7 text-xl">
               Make every collaboration a learning opportunity with improveme.io
             </h3>
-            <p className="mb-7 text-lg">
+            <p className="mb-16 text-lg">
               Our app empowers anyone, regardless of the context or setting, to
               collect feedback from the people they collaborate with. You
               #ownyourfeedback. Use it to improve your performance.
             </p>
-            <Button size={"lg"} className="grow-0 text-lg">
+            <Button size={"lg"} className="grow-0 text-lg bg-sky-700 px-10 py-8 text-white">
               Start Collecting Feedback Now
             </Button>
           </div>
