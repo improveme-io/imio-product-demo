15eb59798779a9b66d37a46d0db8c49c3c15ac4e
robotkutya <mihalyfuredi@gmail.com>
2023-05-10 22:11:40 +0200

    updates clerk auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,11 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default withClerkMiddleware((_req: NextRequest) => {
-  return NextResponse.next();
+export default authMiddleware({
+  ignoredRoutes: [],
+  publicRoutes: ["/"],
 });
 
 // Stop Middleware running on static files
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next
-     * - static (static files)
-     * - favicon.ico (favicon file)
-     */
-    "/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)",
-    "/",
-  ],
+  matcher: ["/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)", "/"],
 };
