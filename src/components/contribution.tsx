3ae2e9e41186556a54ac87a854d7ad3d10a98472
Kristof Gatter <mail@kristofgatter.com>
2023-05-13 20:07:15 +0200

    feat(contribution, feedback-request-item): use card component properly

--- src/components/contribution.tsx
+++ src/components/contribution.tsx
@@ -3,7 +3,7 @@
 import { Button } from "~/components/ui/button";
 import { EditIcon, ViewIcon, CheckIcon } from "lucide-react";
 
-import { Card } from "~/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { Label } from "~/components/ui/label";
 
 import { UserItem } from "~/components/user-item";
@@ -17,30 +17,34 @@ type ContributionProps = {
 };
 export const Contribution = (props: ContributionProps) => {
   return (
-    <Card className="flex flex-col">
-      <div className="mb-4 flex items-center">
-        <UserItem initials={props.requesterInitials} email={props.email} />
+    <Card className="group flex flex-col">
+      <CardHeader>
+        <CardTitle className="text-md flex items-center justify-between font-normal">
+          <UserItem initials={props.requesterInitials} email={props.email} />
+          {props.done ? (
+            <CheckIcon className="ml-auto h-5 w-5 text-green-500" />
+          ) : (
+            <></>
+          )}
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Label>{props.requestName}</Label>
         {props.done ? (
-          <CheckIcon className="ml-auto h-5 w-5 text-green-500" />
+          <Button
+            variant={"outline"}
+            className="mt-4 opacity-0 hover:bg-slate-50 group-hover:opacity-100"
+          >
+            <ViewIcon className="mr-2" />
+            View
+          </Button>
         ) : (
-          <></>
+          <Button className="mt-4">
+            <EditIcon className="mr-2" />
+            Author Feedback
+          </Button>
         )}
-      </div>
-      <Label>{props.requestName}</Label>
-      {props.done ? (
-        <Button
-          variant={"outline"}
-          className="mt-4 opacity-0 hover:bg-slate-50 group-hover:opacity-100"
-        >
-          <ViewIcon className="mr-2" />
-          View
-        </Button>
-      ) : (
-        <Button className="mt-4">
-          <EditIcon className="mr-2" />
-          Author Feedback
-        </Button>
-      )}
+      </CardContent>
     </Card>
   );
 };
--- src/components/feedback-request-item.tsx
+++ src/components/feedback-request-item.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "~/components/ui/button";
 import { EditIcon, ViewIcon, TrashIcon } from "lucide-react";
-import { Card } from "~/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { Label } from "~/components/ui/label";
 import { UserItem } from "./user-item";
 
@@ -13,26 +13,27 @@ type FeedbackRequestItemProps = {
 };
 export const FeedbackRequestItem = (props: FeedbackRequestItemProps) => {
   return (
-    <Card className="mb-8">
-      <div className="flex justify-between">
-        <h3 className="flex text-xl">{props.title}</h3>
-        <p>{props.slug}</p>
-        <div className="flex justify-end gap-3 opacity-0 group-hover:opacity-100">
-          <Button variant="ghost" className="text-red-500">
-            <TrashIcon className="mr-2 h-4 w-4 text-red-500" />
-            Delete
-          </Button>
-          <Button variant="ghost">
-            <EditIcon className="mr-2 h-4 w-4" />
-            Edit
-          </Button>
-          <Button variant="outline">
-            <ViewIcon className="mr-2 h-4 w-4" />
-            Preview
-          </Button>
-        </div>
-      </div>
-      <div className="my-4 flex gap-4">
+    <Card className="group mb-8">
+      <CardHeader>
+        <CardTitle className="flex items-center justify-between">
+          <h3 className="flex text-xl">{props.title}</h3>
+          <div className="flex justify-end gap-3 opacity-0 group-hover:opacity-100">
+            <Button variant="ghost" className="text-red-500">
+              <TrashIcon className="mr-2 h-4 w-4 text-red-500" />
+              Delete
+            </Button>
+            <Button variant="ghost">
+              <EditIcon className="mr-2 h-4 w-4" />
+              Edit
+            </Button>
+            <Button variant="outline">
+              <ViewIcon className="mr-2 h-4 w-4" />
+              Preview
+            </Button>
+          </div>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="my-4 flex gap-4">
         <div className="mb-4 flex flex-grow flex-col">
           <Label>Authors</Label>
           <div className="mt-2 flex">
@@ -41,7 +42,7 @@ export const FeedbackRequestItem = (props: FeedbackRequestItemProps) => {
             ))}
           </div>
         </div>
-      </div>
+      </CardContent>
     </Card>
   );
 };
