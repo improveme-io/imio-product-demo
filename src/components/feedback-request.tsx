d314f53848e3d13ca3c4d4e65c1065b41584a9f8
Kristof Gatter <mail@kristofgatter.com>
2023-05-15 01:17:34 +0200

    feat(feedback-request): add delete confirmation dialog

--- src/components/feedback-request.tsx
+++ src/components/feedback-request.tsx
@@ -5,7 +5,15 @@ import { EditIcon, ViewIcon, TrashIcon } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { Label } from "~/components/ui/label";
 import { UserItem } from "./user-item";
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 type FeedbackRequestProps = {
   title: string | null;
   slug: string;
@@ -19,10 +27,28 @@ export const FeedbackRequest = (props: FeedbackRequestProps) => {
         <div className="flex items-center justify-between">
           <CardTitle>{props.title}</CardTitle>
           <div className="flex justify-end gap-3 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-            <Button variant="ghost" className="text-red-500">
-              <TrashIcon className="mr-2 h-4 w-4 text-red-500" />
-              Delete
-            </Button>
+            <Dialog className="bg-white">
+              <DialogTrigger>
+                <Button variant="ghost" className="text-red-500">
+                  <TrashIcon className="mr-2 h-4 w-4 text-red-500" />
+                  Delete
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+                  <DialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    the Feedback Request <strong>{props.title}</strong>.
+                  </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                  <Button variant="destructive">Delete</Button>
+                  <Button variant={"outline"}>Cancel</Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+
             <Button variant="ghost">
               <EditIcon className="mr-2 h-4 w-4" />
               Edit
