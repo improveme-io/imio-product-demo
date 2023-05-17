2908a495bf966677e34d9d138118fbaf2e2b8ea4
Kristof Gatter <mail@kristofgatter.com>
2023-05-17 23:52:31 +0200

    feat(feedback-request): basic style

--- src/components/feedback-request-view.tsx
+++ src/components/feedback-request-view.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { Label } from "~/components/ui/label";
-
 import { Textarea } from "~/components/ui/textarea";
 
 type FeedbackRequestViewProps = {
@@ -14,20 +12,20 @@ type FeedbackRequestViewProps = {
 export const FeedbackRequestView = (props: FeedbackRequestViewProps) => {
   return (
     <section className="overflow-y-scroll">
-      <CardHeader>
-        <CardTitle>{props.title}</CardTitle>
-      </CardHeader>
-      <CardContent className="flex flex-col px-8 py-8">
-        <h3>{props.paragraph}</h3>
+      <div>
+        <h1 className="p-8 font-serif text-3xl">{props.title}</h1>
+      </div>
+      <div className="flex flex-col px-8 pb-8">
+        <p className="mb-12">{props.paragraph}</p>
         <ul>
           {props.feedbackItems?.map((item, index) => (
             <li key={`feedback-item-${index}`} className="mt-4">
               <Label>{item.prompt}</Label>
-              <Textarea disabled className="mt-2 h-16 font-mono text-xl" />
+              <Textarea disabled className="mt-2 h-96 font-mono text-xl" />
             </li>
           ))}
         </ul>
-      </CardContent>
+      </div>
     </section>
   );
 };
