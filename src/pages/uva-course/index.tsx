e7a3f0f59b3ee20b8e5c5d8c47a47b3d2bb5995c
Kristof Gatter <mail@kristofgatter.com>
2023-05-17 20:55:13 +0200

    feat(uva-course): add about imio section

--- src/pages/uva-course/index.tsx
+++ src/pages/uva-course/index.tsx
@@ -223,142 +223,39 @@ const Home: NextPage = () => {
                 />
               </>
             ))}
-            {/* <Image
-              className=""
-              src="/UI-Illustration-2.png"
-              width={1260}
-              height={1010}
-              alt="Illustration of the UI of the app, showing a screen with sample feedback"
-            />
-            <p className="mb-8 text-lg md:max-w-lg">
-              After sharing your request, you can sit back and wait for the
-              feedback to come in. Once you receive feedback, you can manage it
-              easily view it on the improveme.io platform. As you continue
-              collecting feedback over time, you will have all your feedback in
-              one place - no matter where you worked or with whom. You can then
-              better identify areas for improvement and have a clearer picture
-              of your progress to keep you motivated and growing!
-            </p> */}
-          </div>
-          {/* <div className="flex flex-wrap md:col-span-2">
-            <h2 className="my-12 w-full font-serif text-3xl">Our Team</h2>
-            <div className="mb-16 w-full pr-16 md:w-1/2 lg:w-1/3">
+          <div className="flex flex-wrap md:col-span-2">
+            <h2 className="my-12 w-full font-serif text-3xl">
+              About improveme.io
+            </h2>
+            <p className="mb-8 mr-8 h-24 w-24 md:max-w-lg">
               <Image
-                className="mb-8 mr-8 h-40 w-40 rounded-full bg-green-200 text-3xl md:max-w-lg"
-                src="/PortraitKristof.png"
-                width={350}
-                height={350}
-                alt="Portrait photo of Dr. Brooke Gazdag"
+                className="mx-auto"
+                src="/Logo.svg"
+                width={78}
+                height={60}
+                alt="improveme.io logo"
               />
-              <h3 className="my-12 w-full font-serif text-2xl">
-                Kristof: Product Design and Management
-              </h3>
-              <p className="text-md mb-8 md:max-w-lg">
-                Reuven I. Kristof Gatter builds digital products from the
-                initial brainstorming phase to the final UI design. He takes a
-                holistic approach to his work, adjusting his role according to
-                the project&apos;s needs. Although he is primarily a designer,
-                Kristof has gained considerable experience in mentoring,
-                facilitating workshops, conducting usability tests, and teaching
-                about different aspects of making digital products.
-                <br />
-                <br />
-                He became interested in Feedback and the HR Software space
-                during his tenure at Small Improvements. After having a great
-                experience with peer feedback, he never quite managed to have
-                proper continious feedback at other startups which has lead him
-                to start improveme.io, where feedback can be facilitated without
-                management overhead.
-                <br />
-                <br />
-                Kristof also taught digital design courses at various
-                institutions, including MOME, a design university in Budapest,
-                the University of Arts Berlin, and the Startup Institute.
-              </p>
-            </div>
-            <div className="mb-16 w-full pr-16 md:w-1/2 lg:w-1/3">
-              <Image
-                className="mb-8 mr-8 h-40 w-40 rounded-full bg-green-200 text-3xl md:max-w-lg"
-                src="/PortraitBrooke.png"
-                width={350}
-                height={350}
-                alt="Portrait photo of Dr. Brooke Gazdag"
-              />
-              <h3 className="my-12 w-full font-serif text-2xl">
-                Brooke: Organizational Psychology
-              </h3>
-              <p className="text-md mb-8 md:max-w-lg">
-                Dr. Brooke Gazdag is a dedicated researcher and trainer who has
-                spent the past decade helping others improve their leadership
-                and negotiation skills to enhance their personal and
-                professional lives. <br />
-                <br />
-                Originally from Western New York, she moved to Munich, Germany
-                in 2012 and currently resides in Hamburg. When she is not
-                conducting research on negotiation, leadership, or diversity,
-                Dr. Gazdag conducts negotiation training and leadership
-                development workshops. She finds joy in the creative process of
-                planning new training and working with diverse groups, and
-                welcomes collaboration opportunities. <br />
-                <br />
-                Brooke enjoys working with a broad range of audiences, including
-                working professionals and undergraduate students. In 2019, she
-                was awarded a Teaching Award by her students while working as an
-                Assistant Professor at the Ludwig-Maximilians-Universität
-                München (University of Munich) in the Munich School of
-                Management. In her current role at the University of
-                Amsterdam&apos;s Amsterdam Business School, she has been able to
-                innovate and integrate digital learning tools to enhance the
-                learning experience for her students.
-              </p>
-            </div>
-            <div className="mb-16 w-full pr-16 md:w-1/2 lg:w-1/3">
-              <Image
-                className="mb-8 mr-8 h-40 w-40 rounded-full bg-green-200 text-3xl md:max-w-lg"
-                src="/PortraitMisi.png"
-                width={350}
-                height={350}
-                alt="Portrait photo of Dr. Brooke Gazdag"
-              />
-              <h3 className="my-12 w-full font-serif text-2xl">
-                Mihaly: Software Development
-              </h3>
-              <p className="text-md mb-8 md:max-w-lg">
-                Mihaly Füredi was born in Boston but resides in Budapest. He is
-                a creative thinker who loves code and loves to get things done.
-                He started his career as a customer support agent at Prezi where
-                he fell in love with the product and decided to become an
-                engineer. While picking up calls during a night shift phone
-                support job, he finished a bootcamp-style coding school during
-                the day. This enabled him to advance to a full-stack engineering
-                position that focused on automation and tooling for Prezi&apos;s
-                go-to-market teams. After 4 years, he wanted to explore the
-                freelance space where he worked both as a frontend engineer and
-                as a product owner & scrum master and finally ended up working
-                for various startups in Germany and Austria.
-              </p>
-            </div>
-          </div> */}
-          {/* <div className="flex flex-wrap md:col-span-2">
-            <h2 className="my-12 w-full font-serif text-3xl">Pricing</h2>
-            <p className="mb-8 mr-8 h-24 w-24 rounded-full bg-green-200 px-8 py-8 text-3xl md:max-w-lg">
-              $0
             </p>
             <p className="mb-8 text-lg md:max-w-lg">
-              Just use it and enjoy it. Right now, improveme.io is just a
-              passion project of people who would love more constructive
-              feedback in their lives and is free to use. Depending on what
-              happens in the future, we may plan to incorporate and introduce
-              pricing.
+              improveme.io is a webapp that empowers anyone, regardless of the
+              context or setting, to collect feedback from the people they
+              collaborate with. It is developed as a passion project of people
+              who would love more constructive feedback in their lives and is
+              free to use.
+              <br />
+              <br />
+              <Button size={"lg"} asChild>
+                <Link href="/">Find out more…</Link>
+              </Button>
             </p>
             <Image
-              className="mx-auto w-1/4 "
-              src="/PricingGraphic.png"
-              width={350}
-              height={505}
-              alt="Illustration of a person giving another person a present"
+              className="sm:mt-10 md:mt-0"
+              src="/HeroGraphic.png"
+              width={472}
+              height={584}
+              alt="Graphic depicting three people giving each other feedback in the cloud"
             />
-          </div> */}
+          </div>
         </div>
       </MainLayout>
       <Footer />
