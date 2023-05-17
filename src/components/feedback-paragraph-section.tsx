26bdaf21776747dc9980f9edcc3da49296adeae6
Kristof Gatter <mail@kristofgatter.com>
2023-05-17 23:52:53 +0200

    feat(feedback-paragraph-section): impr placeholder

--- src/components/feedback-paragraph-section.tsx
+++ src/components/feedback-paragraph-section.tsx
@@ -34,7 +34,7 @@ export const FeedbackParagraphSection = () => {
             <>
               <Textarea
                 value={value}
-                placeholder="Now it is time to put into practice what we learned in Week 3 about giving feedback! Being able to give and receive feedback is essential to effective teamwork and personal and professional growth. Remember, receiving feedback is an opportunity for growth and improvement, so approach these questions with an open mind and a willingness to learn from your teammates' perspectives. Please provide specific examples."
+                placeholder="Thanks for filling out my feedback from. Being able to give and receive feedback is essential to effective teamwork and personal and professional growth. Remember, receiving feedback is an opportunity for growth and improvement, so approach these questions with an open mind and a willingness to learn from your teammates' perspectives. Please answer the questions below and always provide specific examples."
                 onChange={(e) => {
                   setValue(e.target.value);
                 }}
