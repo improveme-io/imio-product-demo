2f5117e210e98d2468039588b5e1504ba2dc69a6
Kristof Gatter <mail@kristofgatter.com>
2023-05-18 01:39:52 +0200

    feat(user-item): add classNames prop

--- src/components/user-item.tsx
+++ src/components/user-item.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { cn } from "~/utils/style";
 
 type UserItemProps = {
   email: string | null | undefined;
   initials?: string;
   name?: string;
+  className?: string;
 };
 
 export const UserItem = (props: UserItemProps) => {
   const renderedUsername = props.name ? props.name : props.email;
   return (
-    <li className="mr-6 inline-flex">
+    <li className={cn("mr-6 inline-flex", props.className)}>
       <Avatar className="mr-2 h-6 w-6 text-xs text-slate-500">
         <AvatarImage></AvatarImage>
         <AvatarFallback>{props.initials}</AvatarFallback>
