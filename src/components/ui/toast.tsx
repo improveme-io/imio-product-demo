699eedbe38b32f8556b6974b6497bba5c012f596
Kristof Gatter <mail@kristofgatter.com>
2023-05-18 20:08:17 +0200

    fix(toast):background

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -27,7 +27,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-background border",
+        default: "bg-white border",
         destructive:
           "group destructive border-destructive bg-destructive text-destructive-foreground",
       },
