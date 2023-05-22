3dd178036d39f0d48d96de1090be2e39674615d3
Kristof Gatter <mail@kristofgatter.com>
2023-05-22 17:27:08 +0200

    fix(card): responsive

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "-m-4 rounded-lg border bg-card text-card-foreground shadow-sm sm:m-0",
       className
     )}
     {...props}
