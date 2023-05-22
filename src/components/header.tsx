41636b1059db4ce8648d886ef6a5a0c715ccc5fb
Kristof Gatter <mail@kristofgatter.com>
2023-05-22 16:39:07 +0200

    fix(header): responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,14 +17,17 @@ export const Header = (props: HeaderProps) => {
     <header
       className={cn(
         props.isSmall ? "bg-opacity-70 py-2" : "py-8",
-        "sticky top-0 z-40 flex flex-col bg-stone-100 px-8 transition-all duration-500"
+        "sticky top-0 z-40 flex flex-col overflow-hidden bg-stone-100 px-8 transition-all duration-500"
       )}
     >
-      <div className="flex w-full justify-between">
+      <div className="flex w-full flex-col justify-between sm:flex-row">
         <div className="flex items-center">
           <Link href="/dashboard">
             <Image
-              className="mr-4 transition-all duration-300"
+              className={cn([
+                props.isSmall && "translate-y-3 sm:translate-y-0",
+                "mr-4 transition-all duration-300",
+              ])}
               src="/Logo.svg"
               width={props.isSmall ? 78 / 3 : 78 / 2}
               height={props.isSmall ? 60 / 3 : 60 / 2}
@@ -34,7 +37,7 @@ export const Header = (props: HeaderProps) => {
           <h1
             className={cn(
               "group mr-auto flex font-serif text-3xl tracking-tight transition-transform duration-300 delay-500",
-              props.isSmall && "-translate-y-16",
+              props.isSmall && "hidden sm:flex sm:-translate-y-16",
               !props.isSmall && "-translate-y-0"
             )}
           >
@@ -43,13 +46,15 @@ export const Header = (props: HeaderProps) => {
         </div>
         <div
           className={cn(
-            props.isSmall ? "mr-3" : "mr-6",
-            "ml-auto flex items-center text-right"
+            props.isSmall
+              ? "-mt-3 translate-x-12 sm:mt-3 sm:translate-x-0"
+              : "mt-3 ",
+            "transition-translate flex items-center text-right duration-300 sm:mx-0 sm:ml-auto sm:mr-6 sm:mt-0"
           )}
         >
           {props.children}
         </div>
-        <div className="mt-1 flex flex-col items-end">
+        <div className="absolute right-3 top-3 mt-1 flex flex-col items-end sm:relative sm:right-auto sm:top-auto">
           <UserButton />
         </div>
       </div>
