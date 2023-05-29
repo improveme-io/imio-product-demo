19008c33d9b44144ac621da5d6a944abfe87c126
Kristof Gatter <mail@kristofgatter.com>
2023-05-29 20:16:49 +0200

    fix(user-item): typo

--- src/components/user-item.tsx
+++ src/components/user-item.tsx
@@ -7,7 +7,7 @@ import {
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip";
+} from "~/components/ui/tooltip";
 import { MailIcon } from "lucide-react";
 
 type UserItemProps = {
