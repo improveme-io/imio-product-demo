b4990ceeb0b253e271f9c32688b46a30c5fcdf85
Kristof Gatter <mail@kristofgatter.com>
2025-10-19 14:32:51 +0200

    improves e-mail template and adds preview page

--- src/components/email/feedback-requested.tsx
+++ src/components/email/feedback-requested.tsx
@@ -7,6 +7,7 @@ import {
   Button,
   Link,
   Tailwind,
+  Img,
 } from "@react-email/components";
 import appTailwindConfig from "../../../tailwind.config";
 
@@ -18,44 +19,83 @@ const emailTailwindConfig = {
 
 interface Props {
   authorFirstName: string;
+  ownerProfilePicURL?: string;
   ownerFirstName: string;
   feedbackUrl: string;
+  sessionIntro?: string;
 }
 
 export function FeedbackRequested({
   authorFirstName,
   ownerFirstName,
   feedbackUrl,
+  sessionIntro,
+  ownerProfilePicURL,
 }: Props) {
   return (
     <Html>
       <Tailwind config={emailTailwindConfig}>
-        <Body className="bg-gray-100 p-8">
-          <Container className="rounded-md bg-white p-6">
-            <Heading className="mb-4 text-2xl font-bold text-pink-500">
-              Welcome, {authorFirstName}!
-            </Heading>
+        <Body className="bg-gray-50 p-1">
+          <Container>
+            <div className="p-5">
+              <Img
+                alt="improveme.io"
+                width={312 / 2}
+                src="https://www.improveme.io/HeroLogo.png"
+                className="mb-15"
+              />
 
-            <Text className="mb-6 text-gray-700">
-              You’ve been invited to provide feedback for {ownerFirstName}.
-              Please click the button below to continue.
-            </Text>
+              <div className="flex">
+                {ownerProfilePicURL && (
+                  <div className="h-10 w-10 overflow-clip rounded-full">
+                    <Img
+                      className="[margin:0_auto] w-10"
+                      src={ownerProfilePicURL}
+                    />
+                  </div>
+                )}
 
-            <Button
-              href={feedbackUrl}
-              className="rounded bg-pink-300 px-6 py-3 font-bold text-white no-underline"
-            >
-              Give Your Feedback
-            </Button>
+                <Heading className="mt-1 mb-0 ml-3 text-2xl">
+                  {ownerFirstName} has requested your feedback.
+                </Heading>
+              </div>
 
-            <Text className="mt-6 text-sm text-gray-600">
-              If the button doesn’t work, copy and paste this link into your
-              browser:
-            </Text>
+              <Text className="mb-1 text-gray-700">
+                Hello {authorFirstName},
+              </Text>
 
-            <Link href={feedbackUrl} className="text-blue underline">
-              {feedbackUrl}
-            </Link>
+              <Text className="mb-6 text-gray-700">
+                {ownerFirstName} has asked you to provide feedback using
+                improveme.io — an app that empowers anyone, regardless of the
+                context or setting, to collect feedback from the people they
+                collaborate with.
+              </Text>
+
+              {sessionIntro && (
+                <Text className="mb-6 text-sm text-gray-900">
+                  “{sessionIntro}”
+                </Text>
+              )}
+
+              <Text className="mb-6 text-sm text-gray-700">
+                Please click the button below to continue.
+              </Text>
+
+              <Button
+                href={feedbackUrl}
+                className="text-md grow-0 rounded bg-sky-700 p-4 text-white"
+              >
+                Give Feedback to {ownerFirstName}
+              </Button>
+
+              <Text className="mt-6 text-sm text-gray-600">
+                You can also copy and paste this link into your browser:
+              </Text>
+
+              <Link href={feedbackUrl} className="text-blue underline">
+                {feedbackUrl}
+              </Link>
+            </div>
           </Container>
         </Body>
       </Tailwind>
